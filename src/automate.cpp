12042535378257a18a040694adbc6cc0fed0efd1
robin83470 <robin.renous83@gmail.com>
2026-02-27 09:21:42 +0100

    Etat1

---
+++ src/automate.cpp
@@ -0,0 +1,12 @@
+#include "automate.h"
+#include "symbole.h"
+#include "etat1.h"
+#include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+void Automate::decalage(Symbole * s, Etat1 * e) {
+}
+
