11f68a755f3cb11081378ca0518a9f55967317d4
xamenss <maxenceheurtault04@gmail.com>
2026-02-27 09:24:15 +0100

    ooh

---
+++ include/expr.h
@@ -0,0 +1,9 @@
+#include <map>
+#include <string>
+#inlclude "symbole.h"
+class expr : public symbole {
+    public:
+        expr() : symbole(expr) {};
+        virtual ~expr() {};
+        virtual double eval(const map<string, double>& valeurs) = 0; 
+}
