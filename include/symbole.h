443622c82e07af3d6fb27f121f82c6283bd31831
xamenss <maxenceheurtault04@gmail.com>
2026-02-27 10:17:05 +0100

    oooooh

--- include/symbole.h
+++ include/symbole.h
@@ -5,7 +5,7 @@ using namespace std;
 
 enum Identificateurs { OPENPAR, CLOSEPAR, PLUS, MULT, INT, FIN, ERREUR };
 
-const string Etiquettes[] = { "OPENPAR", "CLOSEPAR", "PLUS", "MULT", "INT", "FIN", "ERREUR" };
+const string Etiquettes[] = { "OPENPAR", "CLOSEPAR", "PLUS", "MULT", "INT", "FIN", "EXPR" };
 
 class Symbole {
    public:
